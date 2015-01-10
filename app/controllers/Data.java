f51b09e8ebfcda774dc28d9a9e054305661e14fa
renexdev <reneczechdev@gmail.com>
2015-01-10 11:36:14 -0300

    basic User class

--- app/controllers/Data.java
+++ app/controllers/Data.java
@@ -3,7 +3,7 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import static play.mvc.Results.*;
-import play.data.*;
+import play.data.*; //Este tiene para el Form  
 import play.libs.*;
 import models.*;
 import play.data.validation.*;
@@ -15,5 +15,17 @@ import java.util.*;
 
 public class Data extends Controller {
 
-    static Form<User> userForm = form(User.class);
+    //static Form<User> userForm = form(User.class);
+    static Form<User> userForm = Form.form(User.class);
+    //The underlying binding is done using Spring data binder
+    public static Result test(){
+        Map<String,String> toBind = new HashMap<String,String>();
+        //Map<String,String> toBind = new HashMap(); //warn uses unchecked or unsafe operations.
+        toBind.put("name","Arturo"); //la primera key tiene que coincidir con la variable de la Clase
+
+        User user = userForm.bind(toBind).get();
+        //https://www.playframework.com/documentation/2.2.x/JavaForms
+        return ok(data.render(user));
+
+    }
 }
